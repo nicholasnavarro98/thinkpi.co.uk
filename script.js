239ac2004fad92924527bebeedc280674ee80502
andubarbu <andubarbu@gmail.com>
2018-02-08 00:23:09 +0000

    further improvements

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(window).scroll(function() {
-var theta = $(window).scrollTop() / 10 % Math.PI;
+var theta = $(window).scrollTop() / 200 % Math.PI;
 $('#logo').css({ transform: 'rotate(' + theta + 'rad)' });
+$('#brand').css({ transform: 'rotate(' + theta + 'rad)' });
+$('#webdes').css({ transform: 'rotate(' + theta + 'rad)' });
+$('#ecom').css({ transform: 'rotate(' + theta + 'rad)' });
+$('#socialm').css({ transform: 'rotate(' + theta + 'rad)' });
 
-$('#brand').css({ transform: 'rotate(-' + theta + 'rad)' });
-$('#webdes').css({ transform: 'rotate(-' + theta + 'rad)' });
-$('#ecom').css({ transform: 'rotate(-' + theta + 'rad)' });
-$('#socialm').css({ transform: 'rotate(-' + theta + 'rad)' });
 
 });
